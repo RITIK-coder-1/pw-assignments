f704ea0e1620a15ce72df2d014d3660c6900e125
RITIK <ritikprofessional111@gmail.com>
2023-11-05 20:34:16 +0530

    completed assignment 7

---
+++ 01-PRACTICE-ASSIGNMENTS/05-JAVASCRIPT/WEEK-13/Javascript-Fundamentals-1/07-Shopping_List/index.js
@@ -0,0 +1,6 @@
+const shoppingList = ["Apples", "Bananas", "Milk", "Bread", "Eggs"];
+
+console.log("Shopping List:");
+for (let i = 0; i < shoppingList.length; i++) {
+  console.log(shoppingList[i]);
+}

21d146b172cf73a61577b99072d0a988e852726b
RITIK <ritikprofessional111@gmail.com>
2023-11-06 21:27:02 +0530

    practising assignment 3

--- 01-PRACTICE-ASSIGNMENTS/05-JAVASCRIPT/WEEK-14/03-Student_List/index.js
+++ 01-PRACTICE-ASSIGNMENTS/05-JAVASCRIPT/WEEK-14/03-Student_List/index.js
@@ -1,23 +1,26 @@
-const students = [
-    { name: "Alice", score: 85 },
-    { name: "Bob", score: 92 },
-    { name: "Charlie", score: 98 },
-    { name: "David", score: 67 },
-    { name: "Eva", score: 73 },
-    { name: "Frank", score: 89 },
-    { name: "Grace", score: 94 },
-    { name: "Hank", score: 76 },
-    { name: "Ivy", score: 81 },
-    { name: "Jack", score: 88 },
-  ];
+// const students = [
+//     { name: "Alice", score: 85 },
+//     { name: "Bob", score: 92 },
+//     { name: "Charlie", score: 98 },
+//     { name: "David", score: 67 },
+//     { name: "Eva", score: 73 },
+//     { name: "Frank", score: 89 },
+//     { name: "Grace", score: 94 },
+//     { name: "Hank", score: 76 },
+//     { name: "Ivy", score: 81 },
+//     { name: "Jack", score: 88 },
+//   ];
 
-  
-function passedStudents(anyArray){
-    for (let index = 0; index < anyArray.length; index++) {
-        if (anyArray[index].score >= 90) {
-            console.log(`Congratulations! ${anyArray[index].name} passed with ${anyArray[index].score} Marks!`);
-        }
-    } 
-}
 
-passedStudents(students)
+// function IspassedStudents(anyArray){
+//     for (let index = 0; index < anyArray.length; index++) {
+//         if (anyArray[index].score >= 90) {
+//             console.log(`Congratulations! ${anyArray[index].name} passed with ${anyArray[index].score} Marks!`);
+//         }
+//         else {
+//             console.log(`Sorry! ${anyArray[index].name}, You could not clear the exam!`);
+//         }
+//     } 
+// }
+
+// IspassedStudents(students) 

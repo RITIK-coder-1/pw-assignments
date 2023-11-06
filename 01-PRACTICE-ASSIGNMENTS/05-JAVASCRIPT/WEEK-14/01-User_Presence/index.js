9b07159c0539157dbe37b9f59d0856a4b263c8a2
RITIK <ritikprofessional111@gmail.com>
2023-11-06 20:42:24 +0530

    finished assignment 1

---
+++ 01-PRACTICE-ASSIGNMENTS/05-JAVASCRIPT/WEEK-14/01-User_Presence/index.js
@@ -0,0 +1,14 @@
+const list1 = [ "Ritik", "Bijay", "Mithun" ]
+const list2 = [ "Sonali", "Sunena", "Sushma" ]
+
+function isUserPresent(anyList, username) {
+    if (anyList.includes(username)){
+        console.log(`Yes, ${username} is a valid user.`);
+    } else {
+        console.log(`${username} is not a valid user!`);
+    }
+}
+
+isUserPresent(list1, "Mithun")
+isUserPresent(list2, "Sushma")
+isUserPresent(list1, "someone")

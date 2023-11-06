4f347abceb2620d89ba3e694d875eaa1cfecab92
RITIK <ritikprofessional111@gmail.com>
2023-11-06 22:57:23 +0530

    finished assignment 8

---
+++ 01-PRACTICE-ASSIGNMENTS/05-JAVASCRIPT/WEEK-14/08-Event_Date/index.js
@@ -0,0 +1,8 @@
+function remainingDays(anyDate) {
+    const currentDate = new Date("Nov 6, 2023") / (1000*60*60*24)
+    return anyDate - currentDate
+}
+
+const eventDate = new Date("Dec 6, 2023") / (1000*60*60*24)
+
+console.log(`The remaining days for the event: ${remainingDays(eventDate)}`);

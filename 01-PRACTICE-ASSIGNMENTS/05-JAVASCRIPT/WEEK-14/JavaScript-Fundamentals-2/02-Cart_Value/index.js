eb30d6a3d72958b32db81fa77e2203acce55208b
RITIK <ritikprofessional111@gmail.com>
2023-11-08 00:42:53 +0530

    completed assigment 2

---
+++ 01-PRACTICE-ASSIGNMENTS/05-JAVASCRIPT/WEEK-14/JavaScript-Fundamentals-2/02-Cart_Value/index.js
@@ -0,0 +1,13 @@
+function totalCartValue(...cart) {
+
+    if (cart.length === 0) {
+      return 0;
+    }
+
+    const sum = cart.reduce((total, num1) => total + num1, 0);
+    let statement = `the total cart value is: ${sum}`
+    console.log(statement);
+    return statement;
+  }
+  
+totalCartValue(125, 20, 30)

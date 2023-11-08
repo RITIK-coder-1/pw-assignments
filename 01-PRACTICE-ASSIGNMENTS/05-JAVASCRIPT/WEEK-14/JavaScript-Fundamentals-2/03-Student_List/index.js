3982378602f7e336bc84f4f7a10792f3441fcdd3
RITIK <ritikprofessional111@gmail.com>
2023-11-08 23:34:19 +0530

    modified assignment 3

--- 01-PRACTICE-ASSIGNMENTS/05-JAVASCRIPT/WEEK-14/JavaScript-Fundamentals-2/03-Student_List/index.js
+++ 01-PRACTICE-ASSIGNMENTS/05-JAVASCRIPT/WEEK-14/JavaScript-Fundamentals-2/03-Student_List/index.js
@@ -1,42 +1,59 @@
-// // Function to check if any student is passed or failed --------->
+// // Program to check if any student is passed or failed --------->
 
 
-// const students = [
-//     { name: "Alice", score: 85 },
-//     { name: "Bob", score: 92 },
-//     { name: "Charlie", score: 98 },
-//     { name: "David", score: 67 },
-//     { name: "Eva", score: 73 },
-//     { name: "Frank", score: 89 },
-//     { name: "Grace", score: 94 },
-//     { name: "Hank", score: 76 },
-//     { name: "Ivy", score: 81 },
-//     { name: "Jack", score: 88 },
-//   ];
+const students = [
+    { name: "Alice", score: 85 },
+    { name: "Bob", score: 92 },
+    { name: "Charlie", score: 98 },
+    { name: "David", score: 67 },
+    { name: "Eva", score: 73 },
+    { name: "Frank", score: 89 },
+    { name: "Grace", score: 94 },
+    { name: "Hank", score: 76 },
+    { name: "Ivy", score: 81 },
+    { name: "Jack", score: 88 },
+  ];
 
+// Function for individual student ---------->
 
-// function IspassedStudents(anyArray){
-//     for (let index = 0; index < anyArray.length; index++) {
-//         if (anyArray[index].score >= 90) {
-//             console.log(`Congratulations! ${anyArray[index].name} passed with ${anyArray[index].score} Marks!`);
-//         }
-//         else {
-//             console.log(`Sorry! ${anyArray[index].name}, You could not clear the exam!`);
-//         }
-//     } 
-// }
+function checkResults(anyString){
 
-// IspassedStudents(students) 
+let isPassed = false
 
-
-
-let array = [{name: "ritik", score: 60}]
-
-for (let index = 0; index < array.length; index++) {
-    if (array[index].hasOwnProperty("name") && array[index].hasOwnProperty("score")) {
-        if (array[index].score > 50) {
-            console.log(array[index].name);
+    for (let index = 0; index < students.length; index++) {
+        if (students[index].hasOwnProperty("name") && students[index].hasOwnProperty("score")) {
+            if (students[index].name === anyString && students[index].score >= 90) {
+                isPassed = true
+            } 
         }
-    }   
+    }  
+
+    if (isPassed){
+        let statement1 = `Congratulations! ${anyString}, You have cleared the exam.`
+        console.log(statement1);
+        return statement1
+    } else {
+        let statement2 = `Sorry ! ${anyString}, You couldn't clear the exam.`
+        console.log(statement2);
+        return statement2
+    }
+
 }
+
+checkResults("Grace")
+
+// Function for displaying all the students ------------>
+
+function studentsResults(anyArray){
+    for (let index = 0; index < anyArray.length; index++) {
+        if (anyArray[index].score >= 90) {
+            console.log(`Congratulations! ${anyArray[index].name} passed with ${anyArray[index].score} Marks!`);
+        }
+        else {
+            console.log(`Sorry! ${anyArray[index].name}, You could not clear the exam!`);
+        }
+    } 
+}
+
+// studentsResults(students) 
 

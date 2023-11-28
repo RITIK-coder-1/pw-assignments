75ba335d1fb79ae9dd751e4a72eb5a7efcdc18c4
RITIK <ritikprofessional111@gmail.com>
2023-11-28 20:47:56 +0530

    finished my 3rd assignment

---
+++ 01-PRACTICE-ASSIGNMENTS/05-JAVASCRIPT/WEEK-15/JavaScript-DOM/02_Theme_Changer/index.js
@@ -0,0 +1,11 @@
+const body = document.body
+const button = document.querySelector("#button")
+const heading = document.querySelector("#heading")
+
+button.addEventListener(("click"), () => {
+
+    body.style.backgroundColor = "black"
+    heading.textContent = "This is Dark Theme"
+    heading.style.color = "white"
+
+})

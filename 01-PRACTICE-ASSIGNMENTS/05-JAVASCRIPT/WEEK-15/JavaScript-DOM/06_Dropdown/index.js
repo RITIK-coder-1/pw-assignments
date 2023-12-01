c5ae80c400be12c97fa8de399bb02b90fcffb1b8
RITIK <ritikprofessional111@gmail.com>
2023-12-01 20:08:59 +0530

    finished my assignment 6

---
+++ 01-PRACTICE-ASSIGNMENTS/05-JAVASCRIPT/WEEK-15/JavaScript-DOM/06_Dropdown/index.js
@@ -0,0 +1,13 @@
+const dropdown = ["JavaScript", "Java", "Python", "C++"]
+
+const option1 = document.querySelector("option[value='option1']")
+option1.append(dropdown[0])
+
+const option2 = document.querySelector("option[value='option2']")
+option2.append(dropdown[1])
+
+const option3 = document.querySelector("option[value='option3']")
+option3.append(dropdown[2])
+
+const option4 = document.querySelector("option[value='option4']")
+option4.append(dropdown[3])

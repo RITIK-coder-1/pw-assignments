0a5bfff52e3667a4cf0bd70734a20899788d205c
RITIK <ritikprofessional111@gmail.com>
2024-02-03 21:09:56 +0530

    completed the 7th assignment of promises

--- 01-PRACTICE-ASSIGNMENTS/05-JAVASCRIPT/WEEK-17/promises/06_fetch/index.js
+++ 01-PRACTICE-ASSIGNMENTS/05-JAVASCRIPT/WEEK-17/promises/06_fetch/index.js
@@ -1,3 +1,7 @@
-fetch("https://jsonplaceholder.typicode.com/todos/1")
-.then(response => response.json())
-.then(data => console.log(data))
+function fetchAPI(API) {
+    fetch(API)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+fetchAPI("https://jsonplaceholder.typicode.com/todos/1")

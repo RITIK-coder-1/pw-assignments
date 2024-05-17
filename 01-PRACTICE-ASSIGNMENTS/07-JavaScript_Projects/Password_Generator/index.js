0fc8948af80e1679ade6f8c5cccf1822ec26e3a5
Ritik Mahapatra <ritikprofessional111@gmail.com>
2024-05-17 21:09:49 +0530

    Initialised the JS code for my password generator

---
+++ 01-PRACTICE-ASSIGNMENTS/07-JavaScript_Projects/Password_Generator/index.js
@@ -0,0 +1,10 @@
+// Selecting Elements --->
+
+const password = document.getElementById("password")
+const generatePassword = document.getElementById("generatePassword")
+const copyToClipboard = document.getElementById("copyToClipboard")
+const passwordLength = document.getElementById("passwordLength")
+const uppercase = document.getElementById("uppercase")
+const lowercase = document.getElementById("lowercase")
+const includeNumbers = document.getElementById("includeNumbers")
+const includeSymbols = document.getElementById("includeSymbols")

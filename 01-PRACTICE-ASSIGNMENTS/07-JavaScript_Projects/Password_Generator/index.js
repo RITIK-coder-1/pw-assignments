3032f8d354496ab4bd74029355b0303829566a5d
Ritik Mahapatra <ritikprofessional111@gmail.com>
2024-05-18 12:02:32 +0530

    My project is 90% done, I have written the main logic, only the include and disinclude part is left

--- 01-PRACTICE-ASSIGNMENTS/07-JavaScript_Projects/Password_Generator/index.js
+++ 01-PRACTICE-ASSIGNMENTS/07-JavaScript_Projects/Password_Generator/index.js
@@ -6,10 +6,17 @@ const copyToClipboard = document.getElementById("copyToClipboard")
 const passwordLength = document.getElementById("passwordLength")
 const uppercase = document.getElementById("uppercase")
 const lowercase = document.getElementById("lowercase")
-const includeNumbers = document.getElementById("includeNumbers")
-const includeSymbols = document.getElementById("includeSymbols")
+const Numbers = document.getElementById("includeNumbers")
+const Symbols = document.getElementById("includeSymbols")
+
+// conditional values -->
 
 let array = [] // this array will contain the password characters
+let includeUppercase = true
+let includeLowercase = true
+let includeNumbers = true
+let includeSymbols = true
+// all of these things will be included at first
 
 // Password String -->
 
@@ -18,16 +25,29 @@ const passwordString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234
 // code logic to generate password -->
 
 generatePassword.addEventListener("click", () => {
-    for (let i = randomNumber(); i < passwordString.length; i++) {
-        const element = passwordString[i];
+    for (let i = 0; i < passwordString.length; i++) {
+        const element = passwordString[i * randomTarget()]; // this helps to shuffle between characters, so that the password is not always linear
         console.log(element)
-        array.push(element) // clicking this button will push the characters to this array
-        password.value = array.toString().replace(/,/g, "") // using the replace method, I replaced all the occurences of a comma. "g" flag replaces all the occurences 
+        array.push(element) // clicking the button will push the characters to this array
+        let stringArray = array.toString().replace(/,/g, "") // using the replace method, I replaced all the occurences of the comma. "g" flag replaces all the occurences 
+        let random = randomNumber() // this stores the return value of the randomNumber() function that is a random number from 0 to 72
+        password.value = stringArray.substring(random, random + 8) // this takes a substring out of the string from a random position and maintains the default length to be 8
         // it will show the array content inside the password box
     }
 }, false)
 
+// function to generate a random number from 0 to 72 -->
+
 function randomNumber() {
-    console.log(Math.floor(Math.random() * 15))
-    return Math.floor(Math.random())
+    let randomNumber = Math.floor(Math.random() * 73)
+    console.log(randomNumber)
+    return randomNumber
 }
+
+// function to generate a random number from 0 to 10 -->
+
+function randomTarget() {
+    let randomNumber = Math.floor(Math.random() * 10)
+    return randomNumber
+}
+

5c0dbf9879e1c25c97167fa95d80c08c7c748315
Ritik Mahapatra <ritikprofessional111@gmail.com>
2024-05-20 20:45:20 +0530

    I finished my password generator assignment completing all the given tasks

--- 01-PRACTICE-ASSIGNMENTS/07-JavaScript_Projects/Password_Generator/index.js
+++ 01-PRACTICE-ASSIGNMENTS/07-JavaScript_Projects/Password_Generator/index.js
@@ -16,7 +16,7 @@ let includeUppercase = true
 let includeLowercase = true
 let includeNumbers = true
 let includeSymbols = true
-// all of these things will be included at first
+// (all of these things will be included at first)
 
 // Password String -->
 
@@ -29,35 +29,29 @@ generatePassword.addEventListener("click", () => {
         const element = passwordString[i * randomTarget()]; // this helps to shuffle between characters, so that the password is not always linear
         console.log(element)
         array.push(element) // clicking the button will push the characters to this array
-        let stringArray = array.toString().replace(/,/g, "") // using the replace method, I replaced all the occurences of the comma. regex "g" flag replaces all the occurences 
-        let random = randomNumber() // this stores the return value of the randomNumber() function that is a random number from 0 to 72
+        let stringArray = array.join("") // this will store the string value of the array content without any space
+        let random = randomNumber() // this stores the return value of the randomNumber() function that is a random number from 0 to 73 (String Length)
+
         // it will show the array content inside the password box -->
+        let passwordValue = password.value = stringArray.substring(random, random + Number(passwordLength.value))  // this takes a substring out of the string from a random position and maintains the length to be whatever is the selected length of the password. I converted the value into a Number, because the default value was a string.
         if (passwordLength.value <= 8) {
             password.value = stringArray.substring(random, random + 8) // this takes a substring out of the string from a random position and maintains the default length to be 8
-        } else {
-            password.value = stringArray.substring(random, random + Number(passwordLength.value)) // if the value is more than 8, that specific number of characters will be displayed. I converted the value into a Number, because the default value was a string.
+        } else if (passwordLength.value > 8 && passwordLength.value <= 73){
+            password.value = passwordValue // if the value is more than 8 and less than 73, that specific number of characters will be displayed.
         } 
         
-        if (!includeUppercase) {
-            password.value = passwordValue.toLowerCase() // if uppercase letters are discluded, all the characters will be in lowercase
-        }
-        if (!includeLowercase){
-            password.value = passwordValue.toUpperCase()
-        }
-        if (!includeNumbers){
-            password.value = passwordValue.replace(/[1234567890]/g, "x")
-        }
-        if (!includeSymbols){
-            password.value = passwordValue.replace(/[!@#$%^&*()_]/g, "z")
-        }
+        password.value = applyAllConditions(passwordValue)
     }
 }, false)
+
 
 // switch to include or not include characters -->
 
 passwordLength.addEventListener("click", () => {
     if (passwordLength.value < 8){
-        alert("The minimun value is 8")
+        alert("The minimun value is 8") // lowering the value will alert the user that the minimum length of the password is 8 and it remains unchanged unless incremented
+    } else if (passwordLength.value > 73){
+        alert("The maximun length is 73") // increasing beyond 73 the value will alert the user that the maximum length of the password is 73 and it remains unchanged unless decremented
     }
 })
 uppercase.addEventListener("click", () => {
@@ -80,7 +74,7 @@ function change(includeX) {
     // I am returning the negated value because String being a primitive data type doesn't change the original value but the copy of it. So, in order to change the original value, I need to store the returned value
 }
 
-// function to generate a random number from 0 to 72 -->
+// function to generate a random number from 0 to 73 -->
 
 function randomNumber() {
     let randomNumber = Math.floor(Math.random() * 73)
@@ -95,3 +89,53 @@ function randomTarget() {
     return randomNumber
 }
 
+// function to include multiple conditionals specified by the user --->
+
+function applyAllConditions(passwordValue) {
+    if (!includeUppercase) {
+        passwordValue = passwordValue.toLowerCase();
+    }
+    if (!includeLowercase) {
+        passwordValue = passwordValue.toUpperCase();
+    }
+    if (!includeNumbers) {
+        passwordValue = passwordValue.replace(/[1234567890]/g, 'x');
+    }
+    if (!includeSymbols) {
+        passwordValue = passwordValue.replace(/[!@#$%^&*()_]/g, 'z');
+    }
+    if (!includeUppercase && !includeLowercase) {
+        passwordValue = passwordValue.replace(/[a-zA-Z]/g, '%');
+    }
+    if (!includeUppercase && !includeNumbers) {
+        passwordValue = passwordValue.toLowerCase().replace(/[1234567890]/g, 'x');
+    }
+    if (!includeUppercase && !includeSymbols) {
+        passwordValue = passwordValue.toLowerCase().replace(/[!@#$%^&*()_]/g, 'z');
+    }
+    if (!includeLowercase && !includeNumbers) {
+        passwordValue = passwordValue.toUpperCase().replace(/[1234567890]/g, 'x');
+    }
+    if (!includeLowercase && !includeSymbols) {
+        passwordValue = passwordValue.toUpperCase().replace(/[!@#$%^&*()_]/g, 'z');
+    }
+    if (!includeNumbers && !includeSymbols) {
+        passwordValue = passwordValue.replace(/[1234567890!@#$%^&*()_]/g, 'r');
+    }
+    if (!includeUppercase && !includeLowercase && !includeNumbers) {
+        passwordValue = passwordValue.replace(/[a-zA-Z0-9]/g, '&');
+    }
+    if (!includeUppercase && !includeLowercase && !includeSymbols) {
+        passwordValue = passwordValue.replace(/[a-zA-Z!@#$%^&*()_]/g, '8');
+    }
+    if (!includeUppercase && !includeNumbers && !includeSymbols) {
+        passwordValue = passwordValue.toLowerCase().replace(/[1234567890!@#$%^&*()_]/g, 'x');
+    }
+    if (!includeLowercase && !includeNumbers && !includeSymbols) {
+        passwordValue = passwordValue.toUpperCase().replace(/[1234567890!@#$%^&*()_]/g, 'z');
+    }
+    if (!includeUppercase && !includeLowercase && !includeNumbers && !includeSymbols) {
+        passwordValue = passwordValue.replace(/[a-zA-Z0-9!@#$%^&*()_]/g, '');
+    }
+    return passwordValue;
+}
